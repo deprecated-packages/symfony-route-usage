da1ce690db1a3a8558f1a7f4417497b4684d23ea
TomasVotruba <tomas.vot@gmail.com>
2020-07-12 13:26:40 +0200

    open 0.4-dev

---
+++ src/Command/ShowDeadRoutesCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\SymfonyRouteUsage\Command;
+
+use Migrify\SymfonyRouteUsage\Routing\DeadRoutesProvider;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Routing\Route;
+use Symplify\PackageBuilder\Console\Command\CommandNaming;
+use Symplify\PackageBuilder\Console\ShellCode;
+
+final class ShowDeadRoutesCommand extends Command
+{
+    /**
+     * @var string[]
+     */
+    private const TABLE_HEADLINE = ['Route Name', 'Controller'];
+
+    /**
+     * @var SymfonyStyle
+     */
+    private $symfonyStyle;
+
+    /**
+     * @var DeadRoutesProvider
+     */
+    private $deadRoutesProvider;
+
+    public function __construct(DeadRoutesProvider $deadRoutesProvider, SymfonyStyle $symfonyStyle)
+    {
+        $this->symfonyStyle = $symfonyStyle;
+        $this->deadRoutesProvider = $deadRoutesProvider;
+
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setName(CommandNaming::classToName(self::class));
+        $this->setDescription('Display dead routes');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $tableData = [];
+        $this->symfonyStyle->title('Dead Routes');
+
+        /** @var Route $route */
+        foreach ($this->deadRoutesProvider->provide() as $routeName => $route) {
+            $tableData[] = [
+                'route_name' => $routeName,
+                'controller' => $route->getDefault('_controller'),
+            ];
+        }
+        $this->symfonyStyle->table(self::TABLE_HEADLINE, $tableData);
+
+        $otherCommandMessage = sprintf(
+            'Do you want to see what routes are used? Run "bin/console %s"',
+            CommandNaming::classToName(ShowRouteUsageCommand::class)
+        );
+        $this->symfonyStyle->note($otherCommandMessage);
+
+        return ShellCode::SUCCESS;
+    }
+}
