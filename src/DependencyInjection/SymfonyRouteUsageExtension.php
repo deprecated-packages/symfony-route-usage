ce6588c653604862c35031e8cbf32fb3737d931b
TomasVotruba <tomas.vot@gmail.com>
2020-07-15 01:30:39 +0200

    switch YAML to PHP configs

--- src/DependencyInjection/SymfonyRouteUsageExtension.php
+++ src/DependencyInjection/SymfonyRouteUsageExtension.php
@@ -8,14 +8,14 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 final class SymfonyRouteUsageExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $containerBuilder): void
     {
-        $loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__ . '/../../config'));
-        $loader->load('config.yaml');
+        $loader = new PhpFileLoader($containerBuilder, new FileLocator(__DIR__ . '/../../config'));
+        $loader->load('config.php');
     }
 
     public function prepend(ContainerBuilder $containerBuilder): void
