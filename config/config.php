d9938dbf523cb994cd07851b86f4c1d76c61d09e
TomasVotruba <tomas.vot@gmail.com>
2020-09-07 03:35:30 +0200

    open 0.4-dev

---
+++ config/config.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\ORM\Tools\SchemaTool;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\PackageBuilder\Console\Style\SymfonyStyleFactory;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set('route_usage_exclude_route_regex', '');
+
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->autowire()
+        ->autoconfigure()
+        ->public();
+
+    $services->load('Migrify\SymfonyRouteUsage\\', __DIR__ . '/../src')
+        ->exclude([__DIR__ . '/../src/Entity', __DIR__ . '/../src/ValueObject']);
+
+    $services->set(SymfonyStyleFactory::class);
+
+    $services->set(SymfonyStyle::class)
+        ->factory([ref(SymfonyStyleFactory::class), 'create']);
+
+    $services->set(SchemaTool::class);
+};
